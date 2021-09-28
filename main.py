f085c216067f1e4510ff5dc8516ced3028e1abaf
Twinxs25 <89766009+Twinxs25@users.noreply.github.com>
2021-09-28 22:14:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,26 @@
-from kivy.metrics import dp
+from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
-from kivymd.uix import textfield
-from kivymd.uix import label
-from kivymd.uix.behaviors import elevation
+from kivymd.uix import menu, screen
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.snackbar import Snackbar
-from kivymd.uix.screen import MDScreen
-from kivymd.app import MDApp
-from kivy.uix.image import Image
-from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.label import MDLabel
-from kivymd.uix.toolbar import MDToolbar
-from kivymd.uix.selectioncontrol import MDSwitch
-from kivymd.uix.toolbar import MDBottomAppBar
+from kivy.metrics import dp
+from kivy.uix.screenmanager import ScreenManager
 
-class WebEx(MDApp):
 
-    def time(self):
-        print("time...")
 
-    def callback(self, button):
-        self.menu.caller = button
-        self.menu.open()
 
-    def menu_callback(self, text_item):
-        self.menu.dismiss()
-        Snackbar(text=text_item).open()
+class Aegis(MDApp):
 
 
     def build(self):
-        
-        screen = MDScreen()
 
-        self.theme_cls.primary_palette = 'Cyan'
+        global screen_manager
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("prelogin.kv"))
+        screen_manager.add_widget(Builder.load_file("menu.kv"))
 
-        #Toolbar with burger and timer functions
-        self.toolbar = MDToolbar(title="THE ANTI-SOCIAL APP",
-        elevation = 8)
-        self.toolbar.pos_hint = {"top": 1}
-        self.toolbar.left_action_items = [
-        ["menu", lambda x: self.callback(x)]]
-        self.toolbar.right_action_items = [["timer", lambda x: self.time()]]        
-        screen.add_widget(self.toolbar)
-
-        #FACEBOOK SWITCH
-        self.switch = MDSwitch(
-        pos_hint = {"center_x" : 0.87, "center_y" : 0.80 })
-        screen.add_widget(self.switch)
-        screen.add_widget(Image(source = "R.png",
-        pos_hint = {"center_x" : 0.75, "center_y" : 0.80},
-        size_hint_x = 0.15, size_hint_y = 0.15))
-
-
-        #YOUTUBE SWITCH
-        self.switch = MDSwitch(
-        pos_hint = {"center_x" : 0.87, "center_y" : 0.60 })
-        screen.add_widget(Image(source = "Y.png",
-        pos_hint = {"center_x" : 0.755, "center_y" : 0.60},
-        size_hint_x = 0.3, size_hint_y = 0.3))
-        
-        screen.add_widget(self.switch)
- 
-
-        #The Loop for the list of websites
         menu_items = [
             {
                 "viewclass": "OneLineListItem",
@@ -77,18 +34,31 @@ class WebEx(MDApp):
             width_mult=3,
         )
 
-        # The text field that users, use to add 
-        # websites to block with URL
-        self.input = MDTextField (
-            hint_text = "Enter Website URL",
-            halign = "center",
-            size_hint = (0.8,1),
-            pos_hint= {"center_x" : 0.5, "center_y" : 0.30},
-            font_size = 22
-        )
-        screen.add_widget(self.input)
-        
-        return screen
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return screen_manager
+
+    def callback(self, button):
+        self.menu.caller = button
+        self.menu.open()
+
+    def menu_callback(self, text_item):
+        self.menu.dismiss()
+        Snackbar(text=text_item).open()
+
+    def time(self):
+        print("time...")
+
+
+    def on_start(self):
+        Clock.schedule_once(self.login, 5)
+
+    def login(self, *args):
+        screen_manager.current = "login"
+
+
+
+
 
 if __name__ == '__main__':
-    WebEx().run()
+    Aegis().run()
